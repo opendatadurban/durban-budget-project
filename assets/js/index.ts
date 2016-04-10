a7c6cc096bbe8055dbaf2747f20ac67d788c21cc
James Smith <mail@jimismith.me>
2016-04-10 15:55:00 +0200

    Formatting changes in index.ts

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -3,19 +3,17 @@ import 'expose?$!expose?jQuery!jquery';
 import * as Rx from 'rxjs';
 import 'bootstrap-webpack';
 
+var pieChart = new PieChart(".chart");
 Promise.resolve($.ajax("/api/budget/sections.json", {
   data: {
     year: "2014/2015"
   }
-}))
-  .then(function(response) {
+})).then(function(response) {
   var expenditure = response.results.map((val) => {
     return new Section(val.title, val.expenditure);
   });
 
-  var pieChart = new PieChart(".chart");
   pieChart.updateData(expenditure);
-})
-  .then(function(data) { }, function(error) {
+}).then(function(data) { }, function(error) {
   console.error(error);
 });
